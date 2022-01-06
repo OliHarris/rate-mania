39a57085fcebcc074d16e32b6ce619957fe70256
OLI-LAPTOP\Oli <revilosirrah@gmail.com>
2022-01-06 11:14:28 +0000

    Updated handler to fix CORS issue

--- rest-api/handler.js
+++ rest-api/handler.js
@@ -1,90 +1,108 @@
-'use strict';
+"use strict";
 
 // top of handler.js
-require('dotenv').config({ path: './variables.env' });
-const connectToDatabase = require('./db');
-const Article = require('./models/Article');
+require("dotenv").config({ path: "./variables.env" });
+const connectToDatabase = require("./db");
+const Article = require("./models/Article");
 
 module.exports.create = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Article.create(JSON.parse(event.body))
-        .then(article => callback(null, {
+  connectToDatabase().then(() => {
+    Article.create(JSON.parse(event.body))
+      .then((article) =>
+        callback(null, {
           statusCode: 200,
           headers: {
-            'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
           },
-          body: JSON.stringify(article)
-        }))
-        .catch(err => callback(null, {
+          body: JSON.stringify(article),
+        })
+      )
+      .catch((err) =>
+        callback(null, {
           statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the article.'
-        }));
-    });
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not create the article.",
+        })
+      );
+  });
 };
 
 module.exports.getOne = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Article.findById(event.pathParameters.id)
-        .then(article => callback(null, {
+  connectToDatabase().then(() => {
+    Article.findById(event.pathParameters.id)
+      .then((article) =>
+        callback(null, {
           statusCode: 200,
           headers: {
-            'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
           },
-          body: JSON.stringify(article)
-        }))
-        .catch(err => callback(null, {
+          body: JSON.stringify(article),
+        })
+      )
+      .catch((err) =>
+        callback(null, {
           statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the article.'
-        }));
-    });
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch the article.",
+        })
+      );
+  });
 };
 
 module.exports.getAll = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Article.find()
-        .then(articles => callback(null, {
+  connectToDatabase().then(() => {
+    Article.find()
+      .then((articles) =>
+        callback(null, {
           statusCode: 200,
           headers: {
-            'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
           },
-          body: JSON.stringify(articles)
-        }))
-        .catch(err => callback(null, {
+          body: JSON.stringify(articles),
+        })
+      )
+      .catch((err) =>
+        callback(null, {
           statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the articles.'
-        }))
-    });
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch the articles.",
+        })
+      );
+  });
 };
 
 module.exports.update = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Article.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-        .then(article => callback(null, {
+  connectToDatabase().then(() => {
+    Article.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {
+      new: true,
+    })
+      .then((article) =>
+        callback(null, {
           statusCode: 200,
           headers: {
-            'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
           },
-          body: JSON.stringify(article)
-        }))
-        .catch(err => callback(null, {
+          body: JSON.stringify(article),
+        })
+      )
+      .catch((err) =>
+        callback(null, {
           statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the articles.'
-        }));
-    });
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not fetch the articles.",
+        })
+      );
+  });
 };
