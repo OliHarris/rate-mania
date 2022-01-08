46cfba54460ad30c2ffbd7f97b3d70c1c24a66a2
OLI-LAPTOP\Oli <revilosirrah@gmail.com>
2022-01-08 21:26:04 +0000

    Added switch to test categories; also overhaul of inital load

--- client/src/App-original.js
+++ client/src/App-original.js
@@ -6,7 +6,6 @@ import Swal from "sweetalert2/src/sweetalert2.js";
 const App = (props) => {
   const [firstLoad, setFirstLoad] = useState(true);
   const [currentArticles, setCurrentArticles] = useState([]);
-  const [currentFilters, setCurrentFilters] = useState([]);
   const [voteCounter, setVoteCounter] = useState(0);
 
   const [articleId, setArticleId] = useState(0);
@@ -38,12 +37,18 @@ const App = (props) => {
 
   const getRandomArticle = useCallback(
     (articlesArray, status, keyword) => {
+      // reset article values
+      setArticleId(0);
+      setArticleTitle("");
+      setArticleExtract("");
+      setArticleFullUrl("");
+      setArticleKeyword("");
+      setArticleImage("");
+
       // only after user input (need to remove article from stack)
       if (keyword) {
         articlesArray = articlesArray.filter((obj) => {
-          console.log(obj.keyword);
-          console.log(decodeURI(keyword));
-          return obj.keyword !== keyword;
+          return obj.name !== decodeURI(keyword);
         });
       }
       const randomArticle =
@@ -51,95 +56,124 @@ const App = (props) => {
 
       // make sure not end of stack!
       if (!(randomArticle === undefined)) {
-        setArticleId(randomArticle.pageid);
-        setArticleTitle(randomArticle.title);
-        setArticleExtract(randomArticle.extract);
-        setArticleFullUrl(randomArticle.fullurl);
-        setArticleKeyword(randomArticle.keyword);
-        setArticleImage(randomArticle.image);
-
-        const getAllUserData = () => {
-          // get MongoDB data
-          // will need to GET from MongoDB (getAll)
-          axios
-            .get(
-              `https://ts1dtr9i4e.execute-api.eu-west-1.amazonaws.com/dev/articles`
-            )
-            .then((response) => {
-              const articlesDb = response.data;
-              if (articlesDb.length) {
-                // reset user stats
-                setDbEntryId("");
-                setUserTotalVotes(0);
-                setUserUnderrated(0);
-                setUserUnderratedPercentage(0);
-                setUserOverrated(0);
-                setUserOverratedPercentage(0);
-
-                const findArticleMatch = (value, callback) => {
-                  if (value.article_id === randomArticle.pageid) {
-                    setDbEntryId(value._id);
-                    setUserTotalVotes(value.underrated + value.overrated);
-                    setUserUnderrated(value.underrated);
-                    setUserUnderratedPercentage(
-                      (value.underrated /
-                        (value.underrated + value.overrated)) *
-                        100
-                    );
-                    setUserOverrated(value.overrated);
-                    setUserOverratedPercentage(
-                      (value.overrated / (value.underrated + value.overrated)) *
-                        100
-                    );
-                    // execute callback when complete
-                    if (callback) callback();
-                  } else {
-                    // execute callback when complete
-                    if (callback) callback();
-                  }
-                };
-
-                // process articles logic
-                let itemsProcessed = 0;
-                // if database already has values
-                articlesDb.forEach((item, index, array) => {
-                  // find matching article already in MongoDB
-                  findArticleMatch(item, () => {
-                    itemsProcessed++;
-                    // callback
-                    if (itemsProcessed === array.length) {
-                      if (status === "voted") {
-                        setVoteCounter(voteCounter + 1);
-                        setCurrentArticles(articlesArray);
-                      } else if (status === "skipped") {
-                        setCurrentArticles(articlesArray);
-                      }
-                      setLoadingState(false);
-                    }
-                  });
+        // get Wikipedia data based on randomArticle.name
+        axios
+          .get(
+            `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&redirects=1&prop=extracts%7Cinfo%7Cpageprops%7Cpageimages&exintro=1&inprop=url&ppprop=page_image&piprop=original&titles=${randomArticle.name}&origin=*`
+          )
+          .then((response) => {
+            const pageData = response.data.query.pages[0];
+            setArticleId(pageData.pageid);
+            setArticleTitle(pageData.title);
+            setArticleExtract(pageData.extract);
+            setArticleFullUrl(pageData.fullurl);
+            setArticleKeyword(pageData.fullurl.split("/").pop());
+            if (pageData.original) {
+              setArticleImage(pageData.original.source);
+            } else if (pageData.pageprops) {
+              // get Wikipedia image based on pageData.pageprops.page_image
+              axios
+                .get(
+                  `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=imageinfo&iiprop=url&titles=File:${pageData.pageprops.page_image}&origin=*`
+                )
+                .then((response) => {
+                  const imageData = response.data.query.pages[0];
+                  setArticleImage(imageData.imageinfo[0].url);
+                })
+                .catch((error) => {
+                  console.log(error);
+                  errorAlert();
                 });
-              } else {
-                // if fresh database
-                if (status === "voted") {
-                  setVoteCounter(voteCounter + 1);
-                  setCurrentArticles(articlesArray);
-                } else if (status === "skipped") {
-                  setCurrentArticles(articlesArray);
-                }
-                setLoadingState(false);
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-              // AWS special error handling - try again if 500
-              if (error.response.status === 500) {
-                getAllUserData();
-              } else {
-                errorAlert();
-              }
-            });
-        };
-        getAllUserData();
+            }
+
+            const getAllUserData = () => {
+              // get MongoDB data
+              // will need to GET from MongoDB (getAll)
+              axios
+                .get(
+                  `https://ts1dtr9i4e.execute-api.eu-west-1.amazonaws.com/dev/articles`
+                )
+                .then((response) => {
+                  const articlesDb = response.data;
+                  if (articlesDb.length) {
+                    // reset user stats
+                    setDbEntryId("");
+                    setUserTotalVotes(0);
+                    setUserUnderrated(0);
+                    setUserUnderratedPercentage(0);
+                    setUserOverrated(0);
+                    setUserOverratedPercentage(0);
+
+                    const findArticleMatch = (value, callback) => {
+                      if (value.article_id === pageData.pageid) {
+                        setDbEntryId(value._id);
+                        setUserTotalVotes(value.underrated + value.overrated);
+                        setUserUnderrated(value.underrated);
+                        setUserUnderratedPercentage(
+                          (value.underrated /
+                            (value.underrated + value.overrated)) *
+                            100
+                        );
+                        setUserOverrated(value.overrated);
+                        setUserOverratedPercentage(
+                          (value.overrated /
+                            (value.underrated + value.overrated)) *
+                            100
+                        );
+                        // execute callback when complete
+                        if (callback) callback();
+                      } else {
+                        // execute callback when complete
+                        if (callback) callback();
+                      }
+                    };
+
+                    // process articles logic
+                    let itemsProcessed = 0;
+                    // if database already has values
+                    articlesDb.forEach((item, index, array) => {
+                      // find matching article already in MongoDB
+                      findArticleMatch(item, () => {
+                        itemsProcessed++;
+                        // callback
+                        if (itemsProcessed === array.length) {
+                          if (status === 'voted') {
+                            setVoteCounter(voteCounter + 1);
+                            setCurrentArticles(articlesArray);
+                          } else if (status === 'skipped') {
+                            setCurrentArticles(articlesArray);
+                          }
+                          setLoadingState(false);
+                        }
+                      });
+                    });
+                  } else {
+                    // if fresh database
+                    if (status === 'voted') {
+                      setVoteCounter(voteCounter + 1);
+                      setCurrentArticles(articlesArray);
+                    } else if (status === 'skipped') {
+                      setCurrentArticles(articlesArray);
+                    }
+                    setLoadingState(false);
+                  }
+                })
+                .catch((error) => {
+                  console.log(error);
+                  // AWS special error handling - try again if 500
+                  if (error.response.status === 500) {
+                    getAllUserData();
+                  } else {
+                    errorAlert();
+                  }
+                });
+            };
+            getAllUserData();
+          })
+          .catch((error) => {
+            console.log(error);
+            errorAlert();
+          });
       } else {
         Swal.fire({
           icon: "success",
@@ -182,119 +216,39 @@ const App = (props) => {
         )
         .then((response) => {
           const articlesArray = [];
-          const filtersArray = [
-            "English-language television shows",
-            "English-language films",
-          ];
-          const filtersString = filtersArray
-            .map((filter) => "Category:" + filter)
-            .join("|");
-
-          // test popular categories output?
-          const testPopularCategories = false;
-          const urlString = testPopularCategories
-            ? "&cllimit=500"
-            : `&clcategories=${filtersString}`;
-          let combinedCategoriesArray = [];
 
           const filterWikiArticles = (value, callback) => {
-            // get Wikipedia data based on value.article
-            axios
-              .get(
-                `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&redirects=1` +
-                  `&prop=extracts|info|pageprops|pageimages|categories` +
-                  `${urlString}` +
-                  `&exintro=1&inprop=url&ppprop=page_image&piprop=original&titles=${value.article}&origin=*`
+            if (
+              // filter name instances amongst returned data
+              !(
+                value.article === "Main_Page" ||
+                value.article.startsWith("Wikipedia:") ||
+                value.article.startsWith("File:") ||
+                value.article.startsWith("Special:") ||
+                value.article.startsWith("Portal:") ||
+                value.article.startsWith("Help:")
               )
-              .then((response) => {
-                const pageData = response.data.query.pages[0];
-
-                if (pageData.categories) {
-                  if (testPopularCategories) {
-                    combinedCategoriesArray = [
-                      ...combinedCategoriesArray,
-                      ...pageData.categories,
-                    ];
-                  }
-
-                  const pushArticleData = (image) => {
-                    articlesArray.push({
-                      pageid: pageData.pageid,
-                      title: pageData.title,
-                      extract: pageData.extract,
-                      fullurl: pageData.fullurl,
-                      keyword: pageData.fullurl.split("/").pop(),
-                      image: image,
-                    });
-                  };
-
-                  // sort image
-                  let dataImage;
-                  if (pageData.original) {
-                    dataImage = pageData.original.source;
-                    pushArticleData(dataImage);
-                    // execute callback when complete
-                    if (callback) callback();
-                  } else if (pageData.pageprops) {
-                    // get Wikipedia image based on pageData.pageprops.page_image
-                    axios
-                      .get(
-                        `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=imageinfo&iiprop=url&titles=File:${pageData.pageprops.page_image}&origin=*`
-                      )
-                      .then((response) => {
-                        const imageData = response.data.query.pages[0];
-                        if (imageData.imageinfo) {
-                          dataImage = imageData.imageinfo[0].url;
-                          pushArticleData(dataImage);
-                          // execute callback when complete
-                          if (callback) callback();
-                        }
-                      })
-                      .catch((error) => {
-                        console.log(error);
-                        errorAlert();
-                      });
-                  } else {
-                    // execute callback when complete
-                    if (callback) callback();
-                  }
-                } else {
-                  // execute callback when complete
-                  if (callback) callback();
-                }
-              })
-              .catch((error) => {
-                console.log(error);
-                errorAlert();
+            ) {
+              articlesArray.push({
+                name: value.article,
               });
+            }
+            // execute callback when complete
+            if (callback) callback();
           };
 
           // filter articles logic
           let itemsProcessed = 0;
           response.data.items[0].articles
-            // return only top 100 (not 1000)
-            // .slice(0, 100)
+            // return only top 500 (not 1000)
+            .slice(0, 100)
             .forEach((item, index, array) => {
               // find matching article already in MongoDB
               filterWikiArticles(item, () => {
                 itemsProcessed++;
                 // callback
                 if (itemsProcessed === array.length) {
-                  // review popular categories output...
-                  if (testPopularCategories) {
-                    let categoryMap = combinedCategoriesArray.reduce((category, val) => {
-                      return category.set(val.title, 1 + (category.get(val.title) || 0));
-                    }, new Map());
-                    const array = Array.from(categoryMap, ([name, value]) => ({
-                      name,
-                      value,
-                    }));
-                    array.sort(function (a, b) {
-                      return b.value - a.value;
-                    });
-                    console.log(array);
-                  }
-
+                  console.log(articlesArray);
                   getRandomArticle(articlesArray, null, null);
                   setCurrentArticles(articlesArray);
                 }
@@ -337,7 +291,7 @@ const App = (props) => {
           newRecord
         )
         .then((response) => {
-          getRandomArticle(currentArticles, "voted", articleKeyword);
+          getRandomArticle(currentArticles, 'voted', articleKeyword);
         })
         .catch((error) => {
           console.log(error);
@@ -373,7 +327,7 @@ const App = (props) => {
           updateRecord
         )
         .then((response) => {
-          getRandomArticle(currentArticles, "voted", articleKeyword);
+          getRandomArticle(currentArticles, 'voted', articleKeyword);
         })
         .catch((error) => {
           console.log(error);
@@ -390,7 +344,7 @@ const App = (props) => {
   // user skip logic
   const handleSkipButton = (event) => {
     setLoadingState(true);
-    getRandomArticle(currentArticles, "skipped", articleKeyword);
+    getRandomArticle(currentArticles, 'skipped', articleKeyword);
   };
 
   return (
