312ea768924344e45a7b0f53b56b4c960d6bbc92
Oliver Harris <revilosirrah@gmail.com>
2023-09-25 21:17:41 +0100

    Fix sweetalert2 styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
-import "@sweetalert2/theme-minimal/minimal.min.css";
 import "./assets/styles/index.css";
 import "./assets/styles/App.scss";
 
